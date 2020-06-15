633943218e9cbed702c4e018e69e9238cacb98ae
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-06-15 19:46:18 -0300

    Adicionando tela de participantes

--- templates/Dashboard/index.php
+++ templates/Dashboard/index.php
@@ -53,6 +53,7 @@
             </ul>
         </nav>
 
+
         <!-- Page Content  -->
         <div id="content">
 
@@ -65,6 +66,7 @@
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
+
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
