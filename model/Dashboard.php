0e675a0a3bb3f7427477fe9e7a7b122c790c79e6
Lucas Oliveira <lucasoliveira.si@outlook.com>
2020-06-16 21:04:27 -0300

    Arrumando rotas

--- model/Dashboard.php
+++ model/Dashboard.php
@@ -1,36 +1,37 @@
 <?php
-class Dashboard {
+class Dashboard
+{
 
-    public function index() {
-
+    public function index()
+    {
     }
 
     public static function render()
     {
+        $dashboardRoute = null;
         if (isset($_GET['dashboardRoute'])) {
             $dashboardRoute = $_GET['dashboardRoute'];
-            switch ($dashboardRoute) {
-                case null:
-                    break;
-                case 'userList':
-                    require "./templates/User/list.php";
-                    break;
-                case 'raffleAdd':
-                    require "./templates/Raffle/addRaffle.php";
-                    break;
-                case 'raffleList':
-                    $raffle = new Raffle();
-                    $data = $raffle->list();
-                    require './templates/Raffle/list.php';
-                    break;
-                case 'raffleEdit':
-                    $raffle = new Raffle();
-                    $data = $raffle->edit();
-                    require './templates/Raffle/edit.php';
-                    break;
-            }
+        }
+        switch ($dashboardRoute) {
+            case null:
+                require "./templates/Dashboard/bodyOfDashboard.php";
+                break;
+            case 'userList':
+                require "./templates/User/list.php";
+                break;
+            case 'raffleAdd':
+                require "./templates/Raffle/addRaffle.php";
+                break;
+            case 'raffleList':
+                $raffle = new Raffle();
+                $data = $raffle->list();
+                require './templates/Raffle/list.php';
+                break;
+            case 'raffleEdit':
+                $raffle = new Raffle();
+                $data = $raffle->edit();
+                require './templates/Raffle/edit.php';
+                break;
         }
     }
-
-
 }
