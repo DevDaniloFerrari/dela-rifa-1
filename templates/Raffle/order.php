4a22dfd14fdef1c1261cc23fea12dc6cb62160ff
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-06-17 21:27:55 -0300

    implementando tela order

--- templates/Raffle/order.php
+++ templates/Raffle/order.php
@@ -1,1 +1,91 @@
-<h1>pedido funciona</h1>
+<head>
+    <link rel="stylesheet" href="./css/cart.css">
+</head>
+
+<body>
+    <div class="container p-5">
+        <form>
+            <div class="row">
+                <div class="form-group col-6">
+                    <label for=""></label>
+                    <input type="text" class="form-control" placeholder="Filtrar por nome do produto">
+                </div>
+                <div class="form-group col-3">
+                    <label for=""></label>
+                    <input type="date" class="form-control">
+                </div>
+                <div class="form-group col-3">
+                    <label for=""></label>
+                    <input type="submit" class="btn btn-success" value="Buscar">
+                </div>
+            </div>
+        </form>
+        <div class="row border">
+            <div class="col-3">
+                <h3 class="text-center">Iphone</h3>
+                <img class="card-img-top" src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946">
+            </div>
+            <div class="col-2 align-self-center text-center">
+                <p><strong>Quantidade:</strong></p>
+                <p><strong>Valor unitário:</strong></p>
+                <p><strong>Valor total:</strong></p>
+                <p><strong>Data da Compra:</strong></p>
+            </div>
+            <div class="col-2 align-self-center text-center">
+                <p>5</p>
+                <p>5</p>
+                <p>R$ 25,00</p>
+                <p>10/05/2020</p>
+            </div>
+            <div class="col-5 align-self-center text-right">
+                <h1><span class="badge badge-secondary"><i class="far fa-clock"></i> Aguardando Sorteio</span></h1>
+            </div>
+        </div>
+        <div class="row border">
+            <div class="col-3">
+                <h3 class="text-center">JBL Boombox</h3>
+                <img class="card-img-top" src="https://www.pontofrio-imagens.com.br/audio/Caixa-de-Som-Portatil/12531579/895071478/caixa-de-som-portatil-jbl-boombox-com-bluetooth-connect-a-prova-dagua-preta-12531579.jpg">
+            </div>
+            <div class=""></div>
+            <div class="col-2 align-self-center text-center">
+                <p><strong>Quantidade:</strong></p>
+                <p><strong>Valor unitário:</strong></p>
+                <p><strong>Valor parcial:</strong></p>
+                <p><strong>Data da Compra:</strong></p>
+            </div>
+            <div class="col-2 align-self-center text-center">
+                <p>10</p>
+                <p>2</p>
+                <p>R$ 20,00</p>
+                <p>03/07/2019</p>
+            </div>
+            <div class="col-5 align-self-center text-right">
+                <h1><span class="badge badge-primary"><i class="far fa-check-circle"></i> Sorteio Realizado</span></h1>
+                <h5>Que pena, <span class="badge badge-warning">Não foi dessa vez!</span></h5>
+            </div>
+        </div>
+        <div class="row border">
+            <div class="col-3">
+                <h3 class="text-center">Play Station 4</h3>
+                <img class="card-img-top" src="https://vignette.wikia.nocookie.net/gta/images/b/bd/PS4.png/revision/latest/top-crop/width/360/height/450?cb=20170321222650&path-prefix=pt">
+            </div>
+            <div class=""></div>
+            <div class="col-2 align-self-center text-center">
+                <p><strong>Quantidade:</strong></p>
+                <p><strong>Valor unitário:</strong></p>
+                <p><strong>Valor parcial:</strong></p>
+                <p><strong>Data da Compra:</strong></p>
+            </div>
+            <div class="col-2 align-self-center text-center">
+                <p>20</p>
+                <p>5</p>
+                <p>R$ 100,00</p>
+                <p>09/03/2020</p>
+            </div>
+            <div class="col-5 align-self-center text-right">
+                <h1><span class="badge badge-primary"><i class="far fa-check-circle"></i> Sorteio Realizado</span></h1>
+                <h5>Parabéns, <span class="badge badge-success">Você ganhou!</span></h5>
+            </div>
+        </div>
+    </div>
+</body>
